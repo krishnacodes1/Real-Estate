70fecfc66cc275342b442ca501a5406a0a17db2d
krishna sahu <krishnasahu000537@gmail.com>
2023-12-01 13:37:25 +0530

    create and add )auth functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -18,6 +18,10 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    avatar:{
+      type:String,
+      default: "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250"
+    },
   },
   { timestamps: true }
 );
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {  signin, signup } from "../controllers/auth.controller.js";
+import {  google, signin, signup } from "../controllers/auth.controller.js";
 // this is for new user or to update user information 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.post('/google', google)
 
 export default router;
