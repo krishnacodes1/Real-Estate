8ed7454a710d5166dd90e407338c0a776c0f3add
krishna sahu <krishnasahu000537@gmail.com>
2023-12-28 12:54:36 +0530

    check avatar

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -58,12 +58,26 @@ export const google = async (req, res, next) => {
           Math.random().toString(36).slice(-4),
         email: req.body.email,
         password: hashedpassword,
+        avatar: req.body.photo,
       });
       await newUser.save();
-      const token = Jwt.sign({id:newUser._id}, process.env.JWT_SECRET);
-      const {password: pass, ...rest} = newUser._doc;
-      res.cookie('access_token',token, {httpOnly:true} ).status(200).json(rest);    }
+      const token = Jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = newUser._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    }
   } catch (error) {
     next(error);
   }
 };
+
+// export const signOut = async (req, res, next) => {
+//   try {
+//     res.clearCookie('access_token');
+//     res.status(200).json('User has been logged out!');
+//   } catch (error) {
+//     next(error);
+//   }
+// };
